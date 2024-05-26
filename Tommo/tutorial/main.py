228ba7928facf6a7b55137562f4703d6fa6610f6
BrownTurtleBear <tommocaddy@gmail.com>
2024-05-26 21:38:29 +0930

    Im sleep now

--- Tommo/tutorial/main.py
+++ Tommo/tutorial/main.py
@@ -16,35 +16,48 @@ pygame.display.set_caption('Gaym')
 clock = pygame.time.Clock()
 
 # font
-font_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/blob/main/Tommo/tutorial/assets/font/Pixeltype.ttf"
-font = pygame.font.Font(None, 50)
+font = pygame.font.Font(r"Tommo\tutorial\assets\font\Pixeltype.ttf", 50)
+
+# text
+text_surface = font.render("Yoooour GAYYY", False, "Black")
 
 # box
 # test_surface = pygame.Surface((screen_width, 100))
 # test_surface.fill('Yellow')
 
 # images
-sky_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/main/Tommo/tutorial/assets/graphics/Sky.png"
-with urllib.request.urlopen(sky_url) as url_response:
-    sky_surface = pygame.image.load(io.BytesIO(url_response.read()))
+sky_surface = pygame.image.load(r"Tommo\tutorial\assets\graphics\Sky.png").convert()
+ground_surface = pygame.image.load(r"Tommo\tutorial\assets\graphics\ground.png").convert()
 
-ground_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/main/Tommo/tutorial/assets/graphics/ground.png"
-with urllib.request.urlopen(ground_url) as url_response:
-    ground_surface = pygame.image.load(io.BytesIO(url_response.read()))
+# moving image
+snail_surface = pygame.image.load(r"Tommo\tutorial\assets\graphics\snail\snail1.png").convert_alpha()
+snail_rectangle = snail_surface.get_rect(midbottom = (0, 300))
 
-# text
-text_surface = font.render("Yoooour GAYYY", False, "Green")
+# using rectangles
+player_surface = pygame.image.load(r"Tommo\tutorial\assets\graphics\Player\player_walk_1.png").convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            if player_rectangle.collidepoint(event.pos):
+                print("stop touching me uwu")
 
     # drawing elements
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(text_surface, (screen_width/2 - text_surface.get_width()/2, 50)) #did this since I did not know rectangles existed lol
+    snail_rectangle.left -= 4
+    if snail_rectangle.left < 0: snail_rectangle.left = screen_width
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
+
+    # collition
+    if player_rectangle.colliderect(snail_rectangle):
+        print('collision')
 
     # update everything
     pygame.display.update()
