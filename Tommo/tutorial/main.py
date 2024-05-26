9d3caa0f66b8f63930631334e95b54c9f22ee7b4
TurtleBear <111428505+BrownTurtleBear@users.noreply.github.com>
2024-05-26 20:44:02 +0930

    more knowleadge

--- Tommo/tutorial/main.py
+++ Tommo/tutorial/main.py
@@ -1,4 +1,6 @@
 import pygame
+import urllib.request
+import io
 from sys import exit
 
 # General setup
@@ -13,12 +15,25 @@ pygame.display.set_caption('Gaym')
 # For Frame rate
 clock = pygame.time.Clock()
 
+# font
+font_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/blob/main/Tommo/tutorial/assets/font/Pixeltype.ttf"
+font = pygame.font.Font(None, 50)
+
 # box
 # test_surface = pygame.Surface((screen_width, 100))
 # test_surface.fill('Yellow')
 
-# image
-test_surface = pygame.image.load("graphics/Sky.png")
+# images
+sky_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/main/Tommo/tutorial/assets/graphics/Sky.png"
+with urllib.request.urlopen(sky_url) as url_response:
+    sky_surface = pygame.image.load(io.BytesIO(url_response.read()))
+
+ground_url = "https://raw.githubusercontent.com/BrownTurtleBear/group-project/main/Tommo/tutorial/assets/graphics/ground.png"
+with urllib.request.urlopen(ground_url) as url_response:
+    ground_surface = pygame.image.load(io.BytesIO(url_response.read()))
+
+# text
+text_surface = font.render("Yoooour GAYYY", False, "Green")
 
 while True:
     for event in pygame.event.get():
@@ -27,7 +42,9 @@ while True:
             exit()
 
     # drawing elements
-    screen.blit(test_surface, (0, (screen_height - 100)))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
 
     # update everything
     pygame.display.update()
