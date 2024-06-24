c32aa6cce2f8d98ce0d9a02cfd381f5237a38488
BrownTurtleBEar <tommocaddy@gmail.com>
2024-06-24 14:21:01 +0930

    folders

--- Iteration 1/ui/cooking.py
+++ Iteration 1/ui/cooking.py
@@ -6,12 +6,11 @@ class Cooking:
     def __init__(self):
         with open(r"Iteration 1\ui\recipes.json", "r") as f:
             self.recipes = json.load(f)
-            
-    
+
     def cook(self, ingredients, food):
-        
+
         needed_ingredients = self.recipes[food]
-        
+
         if all(ni in ingredients for ni in needed_ingredients):
             return True, food
         else:
