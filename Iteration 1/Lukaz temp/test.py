1ba3c91233cdaaa70fc40a26cabfa3131a18a061
LukTeddy <solding.lukaz@gmail.com>
2024-06-27 11:33:04 +0930

    lukaz finishing ui class

--- Iteration 1/Lukaz temp/test.py
+++ Iteration 1/Lukaz temp/test.py
@@ -19,16 +19,16 @@ while True:
     w, h = screen_width - (x * 2), screen_height - (x * 2)
     pygame.draw.rect(screen, "White", pygame.Rect(x, y, w, h))
     if clicked[0] == "main":
-        exit_button = ui.button("rect", screen_width - 70, 10, 60, 30, "Red", None)
+        exit_button = ui.button("rect", False, screen_width - 70, 10, 60, 30, "Red", None)
         if exit_button:
             clicked[0] = "exit"
-        start_button = ui.button("rect", screen_width / 2 - 80, screen_height / 2 - 90, 170, 80, "Green", None)
+        start_button = ui.button("rect", False, screen_width / 2 - 80, screen_height / 2 - 90, 170, 80, "Green", None)
         if start_button:
             clicked[0] = "start"
-        character_button = ui.button("rect", screen_width / 2 - 80, screen_height / 2, 170, 80, "Blue", None)
+        character_button = ui.button("rect", False, screen_width / 2 - 80, screen_height / 2, 170, 80, "Blue", None)
         if character_button:
             clicked[0] = "character"
-        settings_button = ui.button("rect", screen_width / 2 - 80, screen_height / 2 + 90, 170, 80, "Grey", None)
+        settings_button = ui.button("rect", False, screen_width / 2 - 80, screen_height / 2 + 90, 170, 80, "Grey", None)
         if settings_button:
             clicked[0] = "settings"
         ui.text(screen_width / 2, 60, 50, "Black", "Fishing Sim")
@@ -39,14 +39,19 @@ while True:
     if clicked[0] == "settings":
         pygame.draw.rect(screen, "Grey", pygame.Rect(x, y, screen_width - (x * 2), screen_height - (x * 2)))
         ui.text(screen_width / 2, 60, 50, "Black", "Settings Menu")
-        pygame.draw.rect(screen, "White",
-                         pygame.Rect(screen_width / 6, screen_height / 4, screen_width / 3, screen_height / 2))
+        ui.outlined_rect(screen_width / 6, screen_height / 4, screen_width / 3, screen_height / 2, 3, "White")
         ui.text(screen_width / 3, screen_height / 3 - 10, 25, "Black", "Volume")
         ui.text(screen_width / 4 + 35, screen_height / 2 + 10, 25, "Black", str(volume))
-        ui.img_button(90, 210, 50, 45, '../assets/ui/menu/volume.png')
-        up_button = ui.img_button(135, 160, 45, 45, '../assets/ui/menu/arrow.png')
-        down_button = ui.img_button(135, 260, 45, 45, '../assets/ui/menu/arrow.png')
-        exit_button = ui.button(screen_width - 70, 10, 60, 30, "Red", "main")
+        ui.image(90, 210, 50, 45, '../assets/ui/menu/volume.png')
+        up_button = ui.button("img", True, 135, 160, 45, 45, None, '../assets/ui/menu/arrow.png')
+        if up_button and volume < 100:
+            volume += 1
+        down_button = ui.button("img", True, 135, 260, 45, 45, None, '../assets/ui/menu/arrow.png')
+        if down_button and volume > 0:
+            volume -= 1
+        exit_button = ui.button("rect", False, screen_width - 70, 10, 60, 30, "Red", None)
+        if exit_button:
+            clicked[0] = "main"
         ui.text(screen_width - 39, 24, 15, "Black", "Exit")
     if clicked[0] == "character":
         pygame.draw.rect(screen, (3, 7, 252), pygame.Rect(x, y, screen_width - (x * 2), screen_height - (x * 2)))
@@ -55,9 +60,13 @@ while True:
         # Section Boxes
         for i, value in enumerate(ui.sprite_values.keys()):
             if ui.menu_option == value:
-                ui.custom_button((x * 2) + (50 / 2) + (50 * i), 175, 60, 60, '../assets/ui/menu/box1.png', value)
+                button = ui.button("img", True, (x * 2) + (50 / 2) + (50 * i), 175, 60, 60, None,
+                                   '../assets/ui/menu/box1.png')
             else:
-                ui.custom_button((x * 2) + (50 / 2) + (50 * i), 175, 50, 50, '../assets/ui/menu/box1.png', value)
+                button = ui.button("img", True, (x * 2) + (50 / 2) + (50 * i), 175, 50, 50, None,
+                                   '../assets/ui/menu/box1.png')
+                if button:
+                    ui.menu_option = value
         # Colour Boxes
         if ui.menu_option != "Gender":
             ui.outlined_rect(x * 3, screen_height * 0.58,
@@ -78,8 +87,10 @@ while True:
                     temp1 = "Grey"
                 else:
                     temp1 = "Black"
-                ui.button((screen_width - (x * 4) * 0.5) / 1.8 + (25 * x_temp), screen_height * 0.7 + (25 * y_temp),
-                          x * 2, y * 2, value, "character")
+                colour_button = ui.button("rect", False, (screen_width - (x * 4) * 0.5) / 1.8 + (25 * x_temp),
+                                          screen_height * 0.7 + (25 * y_temp), x * 2, y * 2, value, None)
+                if colour_button:
+                    ui.sprite_values[ui.menu_option][0] = value
         else:
             ui.outlined_rect(x*3, screen_height*0.58, (screen_width-(x*6)), screen_height/2.5-x*2, 3, "White")
             ui.text(screen_width / 2, screen_height / 2 + 15, 25, "Black", ui.menu_option)
@@ -90,12 +101,18 @@ while True:
                 ui.text(screen_width * 0.8, screen_height * 0.7, 25, "Black", "None")
                 # Buttons
                 if ui.sprite_values["Gender"] == value:
-                    ui.button(50 + (120 * i), screen_height * 0.75, screen_width / 6, screen_height / 8 - 10, "Green", "character")
-                    ui.text(screen_width * 0.2 + (120 * i), screen_height * 0.8, 25, "Black", "Pick")
+                    gender_button = ui.button("rect", False, 50 + (120 * i), screen_height * 0.75, screen_width / 6,
+                                              screen_height / 8 - 10, "Green", None)
+
                 else:
-                    ui.button(50 + (120 * i), screen_height * 0.75, screen_width / 6, screen_height / 8 - 10, "Red", "character")
-                    ui.text(screen_width * 0.2 + (120 * i), screen_height * 0.8, 25, "Black", "Pick")
-        exit_button = ui.button(screen_width * 0.8, 60, 60, 30, "Red", "main")
+                    gender_button = ui.button("rect", False, 50 + (120 * i), screen_height * 0.75, screen_width / 6,
+                                              screen_height / 8 - 10, "Red", None)
+                ui.text(screen_width * 0.2 + (120 * i), screen_height * 0.8, 25, "Black", "Pick")
+                if gender_button:
+                    ui.sprite_values["Gender"] = value
+        exit_button = ui.button("rect", False, screen_width * 0.8, 60, 60, 30, "Red", None)
+        if exit_button:
+            clicked[0] = "main"
         ui.text(screen_width * 0.875, 74, 15, "Black", "Exit")
     if clicked[0] == "exit":
         pygame.quit()
--- Iteration 1/Lukaz temp/ui.py
+++ Iteration 1/Lukaz temp/ui.py
@@ -6,8 +6,6 @@ from sys import exit
 class UI:
     def __init__(self, screen, clicked):
         self.screen = screen
-        self.sc_width = 0
-        self.sc_height = 0
         self.clicked = clicked
         self.menu_option = "Gender"
         self.sprite_values = {
@@ -38,39 +36,13 @@ class UI:
         text_rect = text.get_rect(center=(x, y))
         self.screen.blit(text, text_rect)
 
-    def custom_button(self, x, y, w, h, location, value):
-        pos = pygame.mouse.get_pos()
-        button = pygame.image.load(location).convert_alpha()
-        button = pygame.transform.scale(button, (w, h))
-        button_rect = button.get_rect(center=(x, y))
-        self.screen.blit(button, button_rect)
-        if button_rect.collidepoint(pos) and self.menu_option != value:
-            button = pygame.transform.scale(button, (w, h))
-            button_rect = button.get_rect(center=(x, y))
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.menu_option = value
-        self.screen.blit(button, button_rect)
-
-    def img_button(self, x, y, w, h, location):
+    def image(self, x, y, w, h, location):
         button = pygame.image.load(location).convert_alpha()
         button = pygame.transform.scale(button, (w, h))
         button_rect = button.get_rect(center=(x, y))
         self.screen.blit(button, button_rect)
 
-    def temp_button1(self, x, y, width, height, colour, type):
-        pos = pygame.mouse.get_pos()
-        button = pygame.draw.rect(self.screen, colour, pygame.Rect(x, y, width, height))
-        pygame.draw.rect(self.screen, "Black", pygame.Rect(x, y, width, height), 1)
-        if button.collidepoint(pos):
-            pygame.draw.rect(self.screen, "Black", pygame.Rect(x, y, width, height), 3)
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.clicked[1] = True
-            elif self.clicked[1]:
-                self.clicked[1] = False
-                self.clicked[0] = type
-        return button
-
-    def temp_button(self, type, x, y, width, height, colour, location, variable, value):
+    def button(self, type, hold, x, y, width, height, colour, location):
         pos = pygame.mouse.get_pos()
         if type == "rect":
             button_rect = pygame.draw.rect(self.screen, colour, pygame.Rect(x, y, width, height))
@@ -89,33 +61,12 @@ class UI:
                 button = pygame.transform.scale(button, (width, height))
                 button_rect = button.get_rect(center=(x, y))
                 self.screen.blit(button, button_rect)
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.clicked[1] = True
-            elif self.clicked[1]:
-                self.clicked[1] = False
-                variable = value
-
-    def button(self, type, x, y, width, height, colour, location):
-        pos = pygame.mouse.get_pos()
-        if type == "rect":
-            button_rect = pygame.draw.rect(self.screen, colour, pygame.Rect(x, y, width, height))
-            pygame.draw.rect(self.screen, "Black", pygame.Rect(x, y, width, height), 1)
-        elif type == "img":
-            button = pygame.image.load(location).convert_alpha()
-            button = pygame.transform.scale(button, (width, height))
-            button_rect = button.get_rect(center=(x, y))
-            self.screen.blit(button, button_rect)
-        else:
-            button, button_rect = None, None
-        if button_rect.collidepoint(pos):
-            if type == "rect":
-                pygame.draw.rect(self.screen, "Black", pygame.Rect(x, y, width, height), 3)
-            elif type == "img":
-                button = pygame.transform.scale(button, (width, height))
-                button_rect = button.get_rect(center=(x, y))
-                self.screen.blit(button, button_rect)
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.clicked[1] = True
-            elif self.clicked[1]:
-                self.clicked[1] = False
-                return True
+            if not hold:
+                if pygame.mouse.get_pressed()[0] == 1:
+                    self.clicked[1] = True
+                elif self.clicked[1]:
+                    self.clicked[1] = False
+                    return True
+            else:
+                if pygame.mouse.get_pressed()[0] == 1:
+                    return True
