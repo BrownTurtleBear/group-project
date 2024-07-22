14479d942b44a9ebc8b8abd15db67e03a47e9438
BrownTurtleBEar <tommocaddy@gmail.com>
2024-07-22 14:59:52 +0930

    tommo - new interation 3

---
+++ Iteration 2/assets/sprites/tile/objects/villiage_tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="villiage_tileset1" tilewidth="16" tileheight="16" tilecount="855" columns="19">
+ <image source="Serene_Village_16x16.png" width="304" height="720"/>
+</tileset>
---
+++ Iteration 2/source_code/items/item.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Item:
+    def __init__(self, name, pos, inventory_pos, description=None):
+        self.name = name
+        self.pos = pos
+        self.inventory_pos = inventory_pos
+        self.description = description
+        self.image = pygame.image.load('../assets/sprites/items/food.png').convert_alpha()
+    
