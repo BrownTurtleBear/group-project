391d41141227fe9c53f9e6e01a7b732732374a41
BrownTurtleBear <tommocaddy@gmail.com>
2024-07-31 22:26:38 +0930

    tommo - changed functions to get_...

--- Iteration 3/source_code/controls/mouse_tracker.py
+++ Iteration 3/source_code/controls/mouse_tracker.py
@@ -8,27 +8,26 @@ class MouseTracker:
 
     def update(self):
         if self.previous_state is None:
-            # First time update is called, initialize both states
             self.previous_state = pygame.mouse.get_pressed()
             self.current_state = self.previous_state
         else:
             self.previous_state = self.current_state
             self.current_state = pygame.mouse.get_pressed()
 
-    def is_button_pressed(self, button=0):
+    def get_button_pressed(self, button=0):
         if self.current_state is None:
             self.update()
         return self.current_state[button]
 
-    def is_button_just_pressed(self, button=0):
+    def get_just_pressed(self, button=0):
         if self.current_state is None or self.previous_state is None:
             self.update()
         return self.current_state[button] and not self.previous_state[button]
 
-    def is_button_just_released(self, button=0):
+    def get_just_released(self, button=0):
         if self.current_state is None or self.previous_state is None:
             self.update()
         return not self.current_state[button] and self.previous_state[button]
 
-# Create a global instance of MouseTracker
+
 mouse_tracker = MouseTracker()
